5abbe0b4f48a9fd1c56aba0f471f6fde049451bf
cui9901 <43556093+cui9901@users.noreply.github.com>
2018-09-29 22:56:55 -0400

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+  import java.util.Scanner;
+
+  public class Temperature 
+  { 
+      public static void main( String args[] ) 
+      { 
+            char a = 'A';
+          	System.out.print((char)(a+1)); 
+         }
+       
+     }
